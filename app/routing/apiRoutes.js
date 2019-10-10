b3cf91e6b9e5fb55fff8f6ba61c17bccfb584316
RecoveringChef <54086786+RecoveringChef@users.noreply.github.com>
2019-10-10 09:21:59 -0400

    routing fixed (again), modal currently commented out

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,36 +17,37 @@ module.exports = function (app) {
         console.log(user);
         console.log(score);
 
-     //do math to compare
-        var friendIndex = 0;
-        var bestFriendName;
-        var bestFriendImage;
-        var bestFriend;
-        var bestFriendScore = 50;
+    //  //do math to compare
+    //     var friendIndex = 0;
+    //     var bestFriendName;
+    //     var bestFriendImage;
+    //     var bestFriend;
+    //     var bestFriendScore = 50;
 
-        function compare(all) {
-            var arrayScore = friends[all].scores;
-            var diff = 0;
+    //     function compare(all) {
+    //         var arrayScore = friends[all].scores;
+    //         var diff = 0;
 
-            for (var i = 0; i < arrayScore.length; i++) { 
-                diff += Math.abs(arrayScore[i] - score[i])  
-            }
-            if (diff < bestFriendScore) {
-                bestFriendScore = diff;
-                bestFriendImage = friends[all].photo;
-                bestFriendName = friends[all].name;
-                bestFriend = friends[all]
-                arrayScore = 0
-                all++
-            } else {
-                diff= 0
-                all++
-            }
-            compare(all)
-        }
-        compare(friendIndex);
-        res.json(friend);
-        user.scores = score;
+    //         for (var i = 0; i < arrayScore.length; i++) { 
+    //             diff += Math.abs(arrayScore[i] - score[i])  
+    //         }
+    //         if (diff < bestFriendScore) {
+    //             bestFriendScore = diff;
+    //             bestFriendImage = friends[all].photo;
+    //             bestFriendName = friends[all].name;
+    //             bestFriend = friends[all]
+    //             arrayScore = 0
+    //             all++
+    //         } else {
+    //             diff= 0
+    //             all++
+    //         }
+    //         compare(all)
+    //     }
+    //     compare(friendIndex);
+       
+    //     user.scores = score;
+            res.json(user);
         friends.push(user);
     });
 }
